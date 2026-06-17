f2d665429fac1b61ebee2000fc3f192ad647dc2e
Mario <mariomorkos98@gmail.com>
2026-06-17 14:11:36 +0200

    <Feat> remplacer le system d'excraction pour avoir une variable

--- extract_csv.py
+++ extract_csv.py
@@ -1,9 +1,22 @@
 import pandas as pd
+import glob
+#import os
+
+#patient_id et nuit_id vont etre manipuler par un input
+patient_id = "1"
+nuit_id = "1"
 
 
-df_patient1 = pd.read_csv("signal-psg-patient-1-nuit-1.csv", encoding="utf-8-sig")
 
-df_patient2 = pd.read_csv("signal-psg-patient-2-nuit-2.csv", encoding="utf-8-sig")
+#BASE_DIR = os.path.dirname(os.path.abspath())
+#csv_path = os.path.join(BASE_DIR, "dossier", "moncsv.csv")
+
+
+csv_patient_1 = glob.glob(f"raw/*{patient_id}*{nuit_id}*.csv")
+
+df_patient1 = pd.read_csv(csv_patient_1[0], encoding="utf-8-sig")
+print(df_patient1)
 
 #exemple pour l'extaction df_patient1["spo2"] => variable["colonne"]
 #pour importer les variables dans autres fichiers => from extract_csv import  df_patient1, df_patient2
+

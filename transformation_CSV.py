33aa3f2e2eef01d6e3bec23b8d9ca49b52e9e8c2
Thomas DUPRESSOIR <thomas.dupressoir@gmail.com>
2026-06-17 16:24:20 +0200

    Données CSV extrappolées

--- transformation_CSV.py
+++ transformation_CSV.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-df = pd.read_csv("signal-psg-patient-1-nuit-1.csv")
+df = pd.read_csv("./raw/signal-psg-patient-1-nuit-1.csv")
 
 # spo2_min = MIN(spo2)
-spo2_min= df["spo2"].min() 
+spo2_min = df["spo2"][0].min() 
 
 # spo2_moy = AVG(spo2)
-spo2_moy = df["spo2"].mean() 
+spo2_moy = df["spo2"][0].mean() 
 
 # spo2_mediane = MEDIAN(spo2)
 spo2_mediane = df["spo2"].median() 
@@ -24,18 +24,23 @@ nb_ronflements_forts = (df["ronflements_db"] > 70).sum()
 position_dominante = df["position"].mode()
 
 #Compter le nombre de secondes où spo2 < 90
-duree_hipoxemie = (df["spo2"] < 90).sum() * 10
+duree_hypoxemie = (df["spo2"] < 90).sum() * 10
 
 
 resultats = {
-    "spo2_min": spo2_min,
-    "spo2_moy": spo2_moy,
-    "spo2_mediane": spo2_mediane,
-    "decibels_max": decibels_max,
-    "decibels_moy": decibels_moy,
-    "position_dominante": position_dominante,
-    "duree_hipoxemie": duree_hipoxemie
+    "spo2_min": df["spo2"].min(),
+    "spo2_moy": df["spo2"].mean(),
+    "spo2_mediane": df["spo2"].median(),
+    "decibels_max": df["ronflements_db"].max(),
+    "decibels_moy": df["ronflements_db"].mean(),
+    "nb_ronflements": ((df["ronflements_db"] > 70).sum()) * 7,
+    "position_dominante": df["position"].mode(),
+    "duree_hypoxemie": (df["spo2"] < 90).sum() * 10
 }
 
-print(resultats)
+# Extrapolation des valeurs pertinentes : nb_ronflements_forts, position_dominante, duree_hypoxemie
+resultats.update([("nb_ronflements", nb_ronflements_forts * 7), ("position_dominante", position_dominante[0]), ("duree_hypoxemie", duree_hypoxemie)])
 
+print("Résultats extrapolés :")
+for cle in resultats:
+    print(cle, ":", resultats.get(cle))

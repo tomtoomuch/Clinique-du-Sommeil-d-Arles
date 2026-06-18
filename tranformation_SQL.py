2a3fb67d885e6891239387f1725d9a540211b742
Thomas DUPRESSOIR <thomas.dupressoir@gmail.com>
2026-06-18 15:15:49 +0200

    procedure sql

--- tranformation_SQL.py
+++ tranformation_SQL.py
@@ -1,4 +1,18 @@
-nbrapneeProc = "exec resultatsnuitsommeil.nbrapnee('id_nuit')"
-nrbhypopnee = "exec resultatsnuitsommeil.nrbhypopnee('id_nuit')"
-nrbrera = "exec resultatsnuitsommeil.nrbrera('id_nuit')"
-nrbtotalevenement = "exec resultatsnuitsommeil.nrbtotalevenement('id_nuit')"
+import mysql.connector
+from extract_csv import nuit_id
+
+nbrapneeProc = "EXEC clinique_sommeil.nbrapnee(?)"
+parametre = (nuit_id)
+
+cnx = mysql.connector.connect(
+    user = 'root',
+    password = '4cc3sB4s3D3D*nn33s',
+    host = 'localhost',
+    database = 'clinique_sommeil',
+    port = '3308'
+)
+
+cur = cnx.cursor(dictionary=True)
+donnees_sql = cur.execute(nbrapneeProc, parametre)
+
+print(donnees_sql)

755894121fe948904ff447109bc7059144455654
Mario <mariomorkos98@gmail.com>
2026-06-18 16:46:47 +0200

    je travail toujours sur le crud <:'(

--- controllersCrud/medecinController.js
+++ controllersCrud/medecinController.js
@@ -1,7 +1,7 @@
 const {getMedecin} = require('../modelsCrud/medecinModel');
 
 exports.afficherTout = async (req,res) => {
-    const tout = await getAllUser();
+    const tout = await getMedecin();
     return res.status(200).json({
             "medecin" : tout
         })
--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const app = express();
 app.use(express.json());
 
 const medecinRoute = require('./routersCrud/medecinRouter')
+ 
 
-app.use('/tout', medecinRoute)
+app.use(medecinRoute);
 
-app.use(router);
+
 app.listen(3000, () => {
     console.log("Serveur démarré sur le port 3000");
 });

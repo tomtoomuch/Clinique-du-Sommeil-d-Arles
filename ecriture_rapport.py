c36b90a79a6de71e10e4cf80649912265f578414
Thomas DUPRESSOIR <thomas.dupressoir@gmail.com>
2026-06-19 10:19:20 +0200

    début d'écriture

---
+++ ecriture_rapport.py
@@ -0,0 +1,8 @@
+from extract_csv import nuit_id, patient_id
+from extract_sql import resultats_nuits
+from transformation_CSV import df_nuit
+import pandas as pd
+
+
+
+with open('./nuit/idnuits/rapport')

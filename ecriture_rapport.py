5827761fae04f8b6786c275f39850de6e591a7cc
Thomas DUPRESSOIR <thomas.dupressoir@gmail.com>
2026-06-19 14:58:39 +0200

    <feat>création dossier + production rapport et écriture dans dossier

--- ecriture_rapport.py
+++ ecriture_rapport.py
@@ -1,18 +1,21 @@
 from extract_csv import nuit_id, patient_id
 from extract_sql import resultat_nuit
 import pandas as pd
+import os
+
+os.mkdir("./nuit/nuit-"+nuit_id+"")
 
 url = "./nuit/nuit-"+nuit_id+"/rapport-patient-"+patient_id+"-nuit-"+nuit_id+".txt"
 
 print(url)
 with open(url, "w", encoding="utf-8-sig") as rapport:
-    rapport.write(f"Patient {resultat_nuit[0]["severite_iah"]} ({resultat_nuit[0]["nom"]}, nuit {resultat_nuit[0]["nom"]})")
-    rapport.write(f"IAH : {resultat_nuit[0]["iah"]}/h → diagnostic SAHOS {resultat_nuit[0]["severite_iah"]}") 
-    rapport.write(f"SpO2 min : {resultat_nuit[0]["spo2_min"]} | moy : {resultat_nuit[0]["spo2_moy"]} | médiane : {resultat_nuit[0]["spo2_mediane"]}")
-    rapport.write(f"nb_apnees : {resultat_nuit[0]["nb_apnees"]} | nb_hypopnees : {resultat_nuit[0]["nb_hypopnees"]} | nb_rera : {resultat_nuit[0]["nb_rera"]} | nb_microeveils : {resultat_nuit[0]["nb_microeveils"]}")
-    rapport.write(f"Position dominante : {resultat_nuit[0]["position_dominante"]}")
-    rapport.write(f"Durée hypoxie : {resultat_nuit[0]["duree_hypoxie_min"]} min")
-    rapport.write(f"Durée apnée moy : {resultat_nuit[0]["duree_apnee_moy_sec"]} | max : {resultat_nuit[0]["duree_apnee_max_sec"]}s")
-    rapport.write(f"Décibels max : {resultat_nuit[0]["decidels_max"]} | moy : {resultat_nuit[0]["decibels_moy"]}")
-    rapport.write(f"Ronflements forts (>70dB) : {resultat_nuit[0]["nb_ronflements_forts"]}")
-    rapport.write(f"Durée sommeil : {resultat_nuit[0]["duree_sommeil_min"]} min")
+    rapport.write(f"Patient {resultat_nuit[0]["severite_iah"]} ({resultat_nuit[0]["nom"]}, nuit {resultat_nuit[0]["nom"]})\n")
+    rapport.write(f"IAH : {resultat_nuit[0]["iah"]}/h → diagnostic SAHOS {resultat_nuit[0]["severite_iah"]}\n") 
+    rapport.write(f"SpO2 min : {resultat_nuit[0]["spo2_min"]} | moy : {resultat_nuit[0]["spo2_moy"]} | médiane : {resultat_nuit[0]["spo2_mediane"]}\n")
+    rapport.write(f"nb_apnees : {resultat_nuit[0]["nb_apnees"]} | nb_hypopnees : {resultat_nuit[0]["nb_hypopnees"]} | nb_rera : {resultat_nuit[0]["nb_rera"]} | nb_microeveils : {resultat_nuit[0]["nb_microeveils"]}\n")
+    rapport.write(f"Position dominante : {resultat_nuit[0]["position_dominante"]}\n")
+    rapport.write(f"Durée hypoxie : {resultat_nuit[0]["duree_hypoxie_min"]} min\n")
+    rapport.write(f"Durée apnée moy : {resultat_nuit[0]["duree_apnee_moy_sec"]} | max : {resultat_nuit[0]["duree_apnee_max_sec"]} s\n")
+    rapport.write(f"Décibels max : {resultat_nuit[0]["decibels_max"]} | moy : {resultat_nuit[0]["decibels_moy"]}\n")
+    rapport.write(f"Ronflements forts (>70dB) : {resultat_nuit[0]["nb_ronflements_forts"]}\n")
+    rapport.write(f"Durée sommeil : {resultat_nuit[0]["duree_sommeil_min"]} min\n")

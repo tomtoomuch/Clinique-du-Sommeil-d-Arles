c8e9f4cd3bda11fddfa826416c5e67b6468c7537
Thomas DUPRESSOIR <thomas.dupressoir@gmail.com>
2026-06-22 15:44:10 +0200

    <fix> route login débuggée

--- db.js
+++ db.js
@@ -1,3 +1,20 @@
+const db = require('mysql2');
+const {motDePasse, bdd, port} = require('./mdp.js')
 
-// modifier les identifiants pour acceder à la BDD
- 
+const connexion = db.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : motDePasse,
+    database: bdd,
+    port: port
+})
+
+connexion.connect((err) => {
+  if (err) {
+    console.error("Erreur connexion MySQL :", err.message);
+    return;
+  }
+  console.log("Connexion MySQL OK");
+});
+
+module.exports = {connexion}

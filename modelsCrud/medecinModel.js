5310eda0d0df46b9f931d1db04c2df1de6c957be
Thomas DUPRESSOIR <thomas.dupressoir@gmail.com>
2026-06-29 16:22:08 +0200

    corrections

--- modelsCrud/medecinModel.js
+++ modelsCrud/medecinModel.js
@@ -1,9 +1,9 @@
-const {connex} = require('../db.js');
-const db = connex;
+const {connexion} = require('../db.js');
+const db = connexion;
 
 function getMedecin(){
     return new Promise ((resolve,reject) => { 
-        db.query('SELECT * from medecin;', (err,rows) => {
+        db.query('SELECT medecin.id_personnel, medecin.specialite, personnel.nom, personnel.prenom FROM medecin LEFT JOIN personnel ON medecin.id_personnel = personnel.id_personnel;', (err,rows) => {
                 if (err){
                         console.log(err.message);
                         return reject(err);
